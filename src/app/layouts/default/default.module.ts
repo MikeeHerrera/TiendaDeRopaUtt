94d6468fdd538cdacf30a3f00fe9a4db8f6b788a
tonygithub-1999 <a3518110423@alumno.uttehuacan.edu.mx>
2021-02-17 20:09:40 -0600

    home modified

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { DefaultComponent } from './default.component';
 import { HomeComponent } from 'src/app/modules/home/home.component';
 import { RopaComponent } from 'src/app/modules/ropa/ropa.component';
